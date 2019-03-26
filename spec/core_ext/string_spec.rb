c2271399729865c5d5787c84253bc27c7256aefa
Jimmy Dee <jgvdthree@gmail.com>
2019-03-26 15:03:33 -0700

    Set up HOME env var in tests for CI

--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -1,4 +1,8 @@
 describe String do
+  before :all do
+    ENV['HOME'] = '/Users/me' if ENV['HOME'].nil?
+  end
+
   it 'obfuscates the home directory' do
     home = ENV['HOME']
     input = "#{home}/abc"
