50d2b545d3201d844d4a400eb30e598feffaedd7
Jimmy Dee <jgvdthree@gmail.com>
2019-03-26 15:55:14 -0700

    more test simplification

--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -5,31 +5,28 @@ describe String do
       ENV['HOME'] ||= "/Users/#{ENV['USER']}"
     end
 
+    let(:home) { ENV['HOME'] }
+    let(:user) { ENV['USER'] }
+
     it 'obfuscates the home directory' do
-      home = ENV['HOME']
       input = "#{home}/abc"
       input.obfuscate!
       expect(input).to eq '~/abc'
     end
 
     it 'obfuscates the username' do
-      user = ENV['USER']
       input = "/a/b/c/#{user}"
       input.obfuscate!
       expect(input).to eq '/a/b/c/$USER'
     end
 
     it 'obfuscates the home directory first' do
-      home = ENV['HOME']
-      user = ENV['USER']
       input = "#{home}/github/#{user}/project"
       input.obfuscate!
       expect(input).to eq '~/github/$USER/project'
     end
 
     it '#obfuscate returns an obfuscated copy' do
-      home = ENV['HOME']
-      user = ENV['USER']
       input = "#{home}/github/#{user}/project"
       output = input.obfuscate
       expect(output).to eq '~/github/$USER/project'
