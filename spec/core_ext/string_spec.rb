9cda203af1d8ac6562af0a2d3091d8b0cfb6b173
Jimmy Dee <jgvdthree@gmail.com>
2019-03-26 15:06:08 -0700

    better test

--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -7,6 +7,6 @@ describe String do
     home = ENV['HOME']
     input = "#{home}/abc"
     input.obfuscate!
-    expect(input).not_to match(/#{Regexp.quote home}/)
+    expect(input).to equal '~/abc'
   end
 end
